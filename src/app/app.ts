f4c5f79bcd686651f84e204e53be4dfc3d620be6
Julio <juliobrsouza@outlook.com>
2026-07-11 22:54:19 -0300

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,41 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import {map, Observable, toArray} from 'rxjs';
+import {AsyncPipe, NgOptimizedImage} from '@angular/common';
+import { environment } from '../environments/environment';
+
+interface Manga {
+  id: number;
+  name: string;
+  mangadex_id: string;
+  cover: string;
+  url: string;
+  status: string;
+  favorite: boolean;
+  created_at: Date;
+  updated_at: Date;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe, NgOptimizedImage],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true
 })
+
 export class App {
-  protected readonly title = signal('manga-angular');
+  private http = inject(HttpClient);
+
+  mangas$: Observable<Manga[]> = fetchManga(this.http);
+}
+
+function fetchManga(http: HttpClient): Observable<Manga[]> {
+  return http.get<Manga[]>(`${environment.apiUrl}/mangas`).pipe(
+    map(mangas =>
+      [...mangas].sort(
+        (a, b) => new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime()
+      ).slice(0, 4)
+    )
+  )
 }
